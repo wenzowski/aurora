5185c5a55df2fd543d0284c3f27bb02f9670ab69
Curtis Chong & Alec Wenzowski <pair+alec+curtis@wenzowski.com>
2016-08-31 19:15:05 +0000

    sql comments start with --

--- init.sql
+++ init.sql
@@ -1,9 +1,10 @@
-# connect to existing database using credentials of db owner
-# psql -f ./init.sql -U skyapi_development_user -h 127.0.0.1 skyapi_development
+-- connect to existing database using credentials of db owner
+-- sudo psql -f ./init.sql -U skyapi_development_user -h 127.0.0.1 skyapi_development
+-- sudo psql -f ./init.sql -U skyapi_test_user -h 127.0.0.1 skyapi_test
 CREATE TABLE level_2_data (
   id SERIAL PRIMARY KEY,
   time TIMESTAMP WITHOUT TIME ZONE,
   point GEOMETRY(Polygon,4326),
   data_fields HSTORE
 );
-# still need compound index level_2_data_time_point
+-- still need compound index level_2_data_time_point
--- setup.sql
+++ setup.sql
@@ -1,8 +1,14 @@
-# make a new user that owns a development database
-# sudo -u postgres psql -f ./setup.sql
+-- make a new user that owns a development database
+-- sudo -u postgres psql -f ./setup.sql
 CREATE USER skyapi_development_user WITH PASSWORD 'pleasefortheloveofspaghettichangeme';
 CREATE DATABASE skyapi_development;
 GRANT ALL PRIVILEGES ON DATABASE skyapi_development to skyapi_development_user;
+CREATE USER skyapi_test_user WITH PASSWORD 'password';
+CREATE DATABASE skyapi_test;
+GRANT ALL PRIVILEGES ON DATABASE skyapi_test to skyapi_test_user;
+\c skyapi_test
+CREATE EXTENSION postgis;
+CREATE EXTENSION hstore;
 \c skyapi_development
 CREATE EXTENSION postgis;
 CREATE EXTENSION hstore;
