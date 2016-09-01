deb14b9010ea37b99c18e86d1db9ce054b01b8e3
Curtis Chong & Alec Wenzowski <pair+alec+curtis@wenzowski.com>
2016-09-01 16:01:00 +0000

    correct column type

--- init.sql
+++ init.sql
@@ -4,7 +4,7 @@
 CREATE TABLE level_2_data (
   id SERIAL PRIMARY KEY,
   time TIMESTAMP WITHOUT TIME ZONE,
-  point GEOMETRY(Polygon,4326),
+  point GEOMETRY(Geometry,4326),
   data_fields HSTORE
 );
 -- still need compound index level_2_data_time_point
